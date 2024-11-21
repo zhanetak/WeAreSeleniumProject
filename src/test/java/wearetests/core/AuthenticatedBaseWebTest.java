c3186dc855581c007c75ed33c92fc83a6d53ab79
JK-telerik <zhaneta.krasteva.a61@learn.telerikacademy.com>
2024-11-21 21:50:41 +0200

    Extend Home page, added CreateAndEditPostTests

--- src/test/java/wearetests/core/AuthenticatedBaseWebTest.java
+++ src/test/java/wearetests/core/AuthenticatedBaseWebTest.java
@@ -4,6 +4,7 @@ import com.weare.pages.LoginPage;
 import jdk.jfr.Label;
 import org.junit.jupiter.api.BeforeEach;
 import wearetests.enums.WEAreTestData;
+import io.qameta.allure.Allure;
 
 public abstract class AuthenticatedBaseWebTest extends BaseWebTest {
 
@@ -16,11 +17,15 @@ public abstract class AuthenticatedBaseWebTest extends BaseWebTest {
 
     protected void authenticateUser() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(
-                WEAreTestData.STANDARD_USER_USERNAME.getValue(),
-                WEAreTestData.STANDARD_USER_PASSWORD.getValue()
-        );
+
+        Allure.step("Logging in as a standard user", () -> {
+            loginPage.login(
+                    WEAreTestData.STANDARD_USER_USERNAME.getValue(),
+                    WEAreTestData.STANDARD_USER_PASSWORD.getValue()
+            );
+        });
+
+        Allure.step("User authenticated successfully.");
         System.out.println("User authenticated successfully.");
     }
 }
-
